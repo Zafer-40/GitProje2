fd1b5ee84586c2c23fa89fd7d59082e9e019b4e8
zafer cansiz <zfrcnsz1@gmail.com>
2023-08-10 19:44:36 +0400

    son guncel

---
+++ src/Gun01/Okul.java
@@ -0,0 +1,10 @@
+package Gun01;
+
+public class Okul {
+    public static void main(String[] args) {
+        Ogrenci ogr1 = new Ogrenci();
+
+        ogr1.soyad = "Temur";
+        ogr1.ad = "asli";
+    }
+}
